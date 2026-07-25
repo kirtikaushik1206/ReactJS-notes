53b689c3883387c7f9da0112583ad58b65a797cb
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-07-25 18:19:47 +0530

    ConditionalStatements

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,13 +1,13 @@
 import "./Product.css"
 
 function Product({title , price , features}){
-    let list = features.map((feature) => <li>{feature}</li>);
+  //  let list = features.map((feature) => <li>{feature}</li>);
     return(
         <div className="Product">
 
             <h3>{title}</h3>
             <h5>Price: {price}</h5>
-             <p> {list}</p>
+             {price >50000 ? <p>Discount of 5%</p> : null}
 
 
 
