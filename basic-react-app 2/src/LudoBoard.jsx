8c605462a09658e9be6912c224a3a2fb108c1607
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-07-31 23:18:24 +0530

    Objects amd State

---
+++ basic-react-app 2/src/LudoBoard.jsx
@@ -0,0 +1,25 @@
+export default function LudoBoard(){
+    return(
+
+
+<div>
+    <p>Game Begins!</p>
+    <div className="board">
+
+        <p>Blue moves = </p>
+       <button>+1</button>
+<p>Yellow moves =</p>
+<button>+1</button>
+ <p>Green moves =</p>
+<button>+1</button>
+ <p>Red moves = </p>
+<button>+1</button>
+
+
+
+    </div>
+</div>
+
+
+    )
+}
