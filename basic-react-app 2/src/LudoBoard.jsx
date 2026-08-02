7a10159fcc54f3a3c823ef7f86d15edcdca057a5
Kirti Kaushik <kirtikaushik1206@gmail.com>
2026-08-02 23:26:01 +0530

    fix: state initialization and move handler in LudoBoard

--- basic-react-app 2/src/LudoBoard.jsx
+++ basic-react-app 2/src/LudoBoard.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react"
+
+
 export default function LudoBoard(){
 
  let [moves , setmoves] = ({blue:0 , red:0 , yellow:0 , green:0 })
-    return(
-
-
+   
+let updateBlue =() =>{
+    console.log(`moves.blue = ${moves.blue}`);
+setmoves(prevmoves) => {
+  return  {...prevmoves , blue: prevmoves.blue +1}
+}
+return(
 <div>
     <p>Game Begins!</p>
     <div className="board">
 
-        <p>Blue moves = </p>
+        <p>Blue moves = {moves.blue}</p>
        <button>+1</button>
-<p>Yellow moves =</p>
+<p>Yellow moves ={moves.yellow}</p>
 <button>+1</button>
- <p>Green moves =</p>
+ <p>Green moves ={moves.green}</p>
 <button>+1</button>
- <p>Red moves = </p>
+ <p>Red moves = {moves.red} </p>
 <button>+1</button>
 
 
